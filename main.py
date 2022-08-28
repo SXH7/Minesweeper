84b9d9c2b80d10664422f690562b3c41b5821ebf
Shashwat Singh <70637141+SXH7@users.noreply.github.com>
2022-08-28 16:19:03 +0530

    obsolete

--- main.py
+++ main.py
@@ -24,12 +24,14 @@ grid = numpy.array([[0, 0, 0, 0, 0],
                     [0, 0, 0, 0, 0]])
 
 mines = 0
+mineLocations = []
 while(mines<=4):
     x = random.randint(0, 4)
     y = random.randint(0, 4)
     if(grid[x, y] == 1):
         continue 
     grid[x, y] = 1
+    mineLocations.append((x, y))
     mines+=1
 
 win = True
@@ -40,14 +42,50 @@ field = numpy.array([["x", "x", "x", "x", "x"],
                     ["x", "x", "x", "x", "x"],
                     ["x", "x", "x", "x", "x"]])
 
+def countMines(cords):
+    total = 0
+    x = cords[0]
+    y = cords[1]
+    coords = [
+        {"x": x-1,  "y": y-1},  
+        {"x": x-1,  "y": y},   
+        {"x": x-1,  "y": y+1}, 
+        {"x": x,    "y": y-1}, 
+        {"x": x,    "y": y+1}, 
+        {"x": x+1,  "y": y-1}, 
+        {"x": x+1,  "y": y},   
+        {"x": x+1,  "y": y+1}, 
+    ]
+    for n in coords:
+        try:
+            if((grid[n["x"]][n["y"]]) == 1):
+                total+=1
+        except KeyError:
+            pass
+    return total
+
+def showMines():
+    for location in mineLocations:
+        field[location] = "O"
+    print(field)
+
+def gameOver():
+    print("You clicked on a mine :<")
+    showMines()
 
 while win:
     print(field)
     turn = []
+    print(grid)
     for x in range(2):
         turn.append(int(input("enter coordinate:")))
+
+    #when game over
     if((grid[turn[0]])[turn[1]]):
-        print("You clicked on a mine :<")
-        print(grid)
-        win == False
+        gameOver()
         break
+
+    else:
+        print(grid)    #only for testing
+        print(countMines(turn))
+        
