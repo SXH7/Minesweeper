969fb90645a446fe616e7505af923ce34d026725
Shashwat Singh <70637141+SXH7@users.noreply.github.com>
2022-09-22 17:14:14 +0530

    commit for backup

--- gui.py
+++ gui.py
@@ -6,22 +6,25 @@ window = pygame.display.set_mode((700, 700))
 window.fill((60,25,60))
 pygame.display.set_caption("Minesweeper")
 
+menusprites = [pygame.image.load("Assets\Art\play.png"), pygame.image.load("Assets\Art\quit.png")]
+
 def drawmenu():
-    play = pygame.image.load("Assets\Art\play.png")
-    quit = pygame.image.load("Assets\Art\quit.png")
-    window.blit(play, (150, 150))
-    window.blit(quit, (150, 350))
+    window.blit(menusprites[0], (150, 150))
+    window.blit(menusprites[1], (150, 350))
+    
 
 
 run = True
 while run:
+    drawmenu()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-        if event.type == pygame.MOUSEBUTTONDOWN:
+        '''if event.type == pygame.MOUSEBUTTONDOWN:
             pos = pygame.mouse.get_pos()
-            print(pos)
-    drawmenu()
+            for s in menusprites:
+                if s.rect.collidepoint(pos):
+                    print('e')'''
     pygame.display.update()
 
 pygame.quit()
