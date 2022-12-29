25dc2bcf0208a30800b52b35413f68f39dd8a364
Shashwat Singh <70637141+SXH7@users.noreply.github.com>
2022-12-29 23:14:19 +0530

    Update textonly.cpp

--- CPP/textonly.cpp
+++ CPP/textonly.cpp
@@ -1,68 +1,69 @@
 #include<iostream>
-#include<cstdlib>
 #include<time.h>
 
 using namespace std;
 
+int SIDE;
 
-int main(){
-    int size;
+void placeMines(int grid[][50], char field[][50], int mines[][2]){
 
-    cout << "Enter size of grid: " ;
-    cin >> size;
-    cout << endl;
+    srand(time (NULL));
 
-    int grid[size][size], counter1 = 0, counter2 = 0, xcords[size], ycords[size], x, y;
-    char field[size][size];
-    bool check;
-    
+	for (int i=0; i<SIDE; i++){
+		for (int j=0; j<SIDE; j++){
+			grid[i][j] = 0;
+            field[i][j] = 'x';
+		}
+	}
 
-    // Initializes the grid and field
-    while(counter1 < size){
-        counter2 = 0;
-        while(counter2 < size){
-            field[counter1][counter2] = 'x';
-            grid[counter1][counter2] = 0;
-            counter2++;
-        }
-        counter1++;
-    }
-
-    counter1 = 0, counter2 = 0;
-
-
-    // Adds mines and checks there are no duplicates
-    srand(time(0));
-    while(counter1 < size){
-        check = false;
-        counter2 = 0;
-        x = rand()%size;
-        y = rand()%size;
-        while(counter2 < size){
-            if(xcords[counter2] == x && ycords[counter2] == y){
-                check = true;
-            }
-            counter2++;
-        }
-        if(check){
+    for(int i = 0; i<SIDE;){
+        int x = rand()%SIDE;
+        int y = rand()%SIDE;
+        if(grid[x][y]){
             continue;
         }
-        xcords[counter1] = x;
-        ycords[counter1] = y;
+        mines[i][0] = x;
+        mines[i][1] = y;
         grid[x][y] = 1;
-        counter1++;
-
+        i++;
     }
+}
 
-    counter1 = 0, counter2 = 0;
-    while(counter1<size){
-            counter2 = 0; 
-            while(counter2 < size){
-                cout << grid[counter1][counter2] << " ";
-                counter2++;
-            }
-            cout << endl;
-            counter1++;
+void showField(char field[][50]){
+    for(int i = 0; i < SIDE; i++){
+        for(int j = 0; j < SIDE; j++){
+            cout << field[i][j] << " ";
+        }
+        cout << endl;
     }
+}
+
+int main(){
+
+    bool game = true;
+    int grid[50][50], mines[50][2], x, y;
+    char field[50][50];
+
+    cout << "Enter the number of sides(cannot be more than 99): ";
+    cin >> SIDE;
+
+    placeMines(grid, field, mines);
     
+    for(int i = 0; i < SIDE; i++){
+        for(int j = 0; j < SIDE; j++){
+            cout << grid[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    while(game){
+        showField(field);
+        cout << "Enter the X value: ";
+        cin >> x;
+        cout << "Enter the Y value: ";
+        cin >> y;
+
+
+    }
+
 }
